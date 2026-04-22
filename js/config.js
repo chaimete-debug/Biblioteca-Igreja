e9d5eb5d83640f0ee0b967f44476432fa797e2cf
chaimete-debug <chaimete@gmail.com>
2026-04-23 01:36:27 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,4 +5,4 @@
 // ─────────────────────────────────────────────────────────────
 
 window.SUPABASE_URL  = 'https://xmuyahwlyerplsxrodup.supabase.co';
-window.SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtdXlhaHdseWVycGxzeHJvZHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MDIyNzAsImV4cCI6MjA2MDk3ODI3MH0.PARTE_FINAL';
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtdXlhaHdseWVycGxzeHJvZHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4ODYyNzIsImV4cCI6MjA5MjQ2MjI3Mn0.6EEUHj0bwbLweC8f_xet7AGa6iQP34nAJbncYx0bBkE
